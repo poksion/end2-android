ce12df15007ebe71473651573b6a49bf21883c62
poksion <poksion@gmail.com>
2014-12-20 10:07:37 +0900

    Extract end2 to another repository

---
+++ jni/README.txt
@@ -0,0 +1,1 @@
+Please check out end2 in this directory from github (https://github.com/poksion/end2.git)
--- jni/end2/src/main/scenegraph/math/EMath.cpp
+++
@@ -1,8 +0,0 @@
-#include "EMath.h"
-
-namespace end2 {
-
-const float EMath::PI = 3.14159265359f;
-const float EMath::HALF_PI = EMath::PI * 0.5f;
-
-}
